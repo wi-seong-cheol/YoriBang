9962cb26aec51a0013065cf236b5376dc80b0365
wi-seong-cheol <tjd9248@naver.com>
2022-06-22 23:23:00 +0900

    update

---
+++ YoriBang/Model/RecipeModel.swift
@@ -0,0 +1,19 @@
+//
+//  RecipeModel.swift
+//  YoriBang
+//
+//  Created by wi_seong on 2021/11/27.
+//
+
+import Foundation
+import UIKit
+
+struct RecipeModel {
+    var thumbNail: UIImage
+    var title: String
+    var writer: UserModel
+    var create: String
+    var update: String
+    var cookTime: String
+    var id: String
+}
